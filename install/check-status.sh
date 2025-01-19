13e4653f249be656b0134faa20eeb35bcdb673dc
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-01-19 14:25:02 +0530

    Update installation status check script

--- install/check-status.sh
+++ install/check-status.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Define color codes for console formatting
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+DARK_YELLOW="\033[0;33m"
+RESET="\033[0m"
+
 # Namespace where all dependencies are installed
 NAMESPACE="choreo-system-dp"
 
 # List of dependencies
-dependencies=("cilium-agent" "cilium-operator" "vault" "vault-agent-injector" "argo-workflows-server" "argo-workflows-workflow-controller" "ingress-nginx")
+dependencies=("cilium-agent" "cilium-operator" "vault" "vault-agent-injector" "argo-workflows-server" "argo-workflows-workflow-controller" "cert-manager" "cainjector" "webhook" "choreo-controllers" "gateway-helm" )
 
 # Function to check the status of a dependency
 check_status() {
@@ -32,5 +38,11 @@ check_status() {
 echo "Installation status:"
 for dependency in "${dependencies[@]}"; do
     status=$(check_status "$dependency")
-    echo "- $dependency : $status"
+    if [[ "$status" =~ "ready" ]]; then
+        echo -e "${GREEN}✅ $dependency : $status ${RESET}"
+    elif [[ "$status" =~ "unknown" ]]; then
+        echo -e "${RED}⚠ $dependency : $status ${RESET}"
+    elif [[ "$status" =~ "pending" ]]; then
+        echo -e "${DARK_YELLOW}🕑 $dependency : $status ${RESET}"
+    fi
 done
