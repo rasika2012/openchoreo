06a0ca352dae2e8663df7405953a3a7dc1023e89
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-02-04 06:46:42 +0530

    Fix sample webapp build failure & remove duplicate project CR

--- internal/controller/build/workflow.go
+++ internal/controller/build/workflow.go
@@ -328,7 +328,7 @@ podman volume prune --force`, imageName, build.Spec.Path, getLanguageVersion(bui
 	} else {
 		buildScript = fmt.Sprintf(`
 podman build -t %s-{{inputs.parameters.git-revision}} -f /mnt/vol/source%s /mnt/vol/source%s
-podman save -o /mnt/vol/app-image.tar %s--{{inputs.parameters.git-revision}}`, imageName, getDockerfilePath(build), getDockerContext(build), imageName)
+podman save -o /mnt/vol/app-image.tar %s-{{inputs.parameters.git-revision}}`, imageName, getDockerfilePath(build), getDockerContext(build), imageName)
 	}
 
 	return []string{baseScript + buildScript}
