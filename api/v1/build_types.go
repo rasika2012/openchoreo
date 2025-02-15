6be9e8d72e7343031eb37437cf792f0cade4c9f6
chalindkodikara <chalindu@wso2.com>
2025-02-15 23:01:33 +0530

    Add react buildpack type to build cr

--- api/v1/build_types.go
+++ api/v1/build_types.go
@@ -42,6 +42,7 @@ type BuildEnvironment struct {
 type BuildpackName string
 
 const (
+	BuildpackReact  BuildpackName = "React"
 	BuildpackGo     BuildpackName = "Go"
 	BuildpackJava   BuildpackName = "Java"
 	BuildpackNodeJS BuildpackName = "Node.js"
@@ -53,6 +54,7 @@ const (
 // SupportedVersions maps each buildpack to its supported versions.
 // Refer (builder:google-22): https://cloud.google.com/docs/buildpacks/builders
 var SupportedVersions = map[BuildpackName][]string{
+	BuildpackReact:  {"23.7.0", "22.14.0", "21.7.3", "20.18.3", "19.9.0", "18.20.6", "17.9.1", "16.20.2"},
 	BuildpackGo:     {"1.x"},
 	BuildpackJava:   {"8", "11", "17", "18", "21"},
 	BuildpackNodeJS: {"12.x.x", "14.x.x", "16.x.x", "18.x.x", "20.x.x", "22.x.x"},
