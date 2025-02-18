4e04c67ad2c030eeed3f575db8b275eb56b6501d
Miraj Abeysekara <miraj@wso2.com>
2025-02-18 19:16:06 +0530

    Update the name len limit in endpoint controller

--- internal/controller/endpoint/integrations/kubernetes/httproute_handler.go
+++ internal/controller/endpoint/integrations/kubernetes/httproute_handler.go
@@ -176,11 +176,11 @@ func makeNamespaceName(endpointCtx *dataplane.EndpointContext) string {
 	organizationName := controller.GetOrganizationName(endpointCtx.Project)
 	projectName := controller.GetName(endpointCtx.Project)
 	environmentName := controller.GetName(endpointCtx.Environment)
-	return dpkubernetes.GenerateK8sName("dp", organizationName, projectName, environmentName)
+	return dpkubernetes.GenerateK8sNameWithLengthLimit(dpkubernetes.MaxNamespaceNameLength, "dp", organizationName, projectName, environmentName)
 }
 
 func makeServiceName(deployCtx *dataplane.EndpointContext) string {
 	componentName := deployCtx.Component.Name
 	deploymentTrackName := deployCtx.DeploymentTrack.Name
-	return dpkubernetes.GenerateK8sName(componentName, deploymentTrackName)
+	return dpkubernetes.GenerateK8sNameWithLengthLimit(dpkubernetes.MaxServiceNameLength, componentName, deploymentTrackName)
 }
