9f76e9904bb2333896a368610b793cea211bd269
jhivandb <jhivanb@gmail.com>
2025-03-03 12:48:48 +0530

    Adds license and cleans up code

--- internal/controller/endpoint/integrations/kubernetes/security_policy_handler.go
+++ internal/controller/endpoint/integrations/kubernetes/security_policy_handler.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2025, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package kubernetes
 
 import (
@@ -16,8 +34,6 @@ import (
 	choreov1 "github.com/wso2-enterprise/choreo-cp-declarative-api/api/v1"
 	"github.com/wso2-enterprise/choreo-cp-declarative-api/internal/dataplane"
 )
-
-// TODO Add error logging
 
 type SecurityPolicyHandler struct {
 	client client.Client
@@ -94,8 +110,6 @@ func makeSecurityPolicy(epCtx *dataplane.EndpointContext) *egv1a1.SecurityPolicy
 }
 
 func makeSecurityPolicySpec(epCtx *dataplane.EndpointContext) egv1a1.SecurityPolicySpec {
-	// Find a better way TODO
-	// idpUrl := "idp.choreo-system.svc.cluster.local:8080/.well-known/jwks.json"
 	return egv1a1.SecurityPolicySpec{
 		JWT: &egv1a1.JWT{
 			Providers: []egv1a1.JWTProvider{
