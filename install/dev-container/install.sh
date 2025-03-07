fb393bae9c93611b0fb0464e18ddc1dac02feadf
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-03-07 07:43:40 +0530

    Expose external gateway to host machine

--- install/dev-container/install.sh
+++ install/dev-container/install.sh
@@ -19,5 +19,18 @@ fi
 terraform -chdir=terraform init -upgrade
 terraform -chdir=terraform apply -auto-approve
 
+echo "Finding external gateway nodeport..."
+NODEPORT=$(kubectl get svc -n choreo-system -l gateway.envoyproxy.io/owning-gateway-name=gateway-external \
+  -o jsonpath='{.items[0].spec.ports[0].nodePort}')
+
+if [[ -z "$NODEPORT" ]]; then
+  echo "Error: Could not retrieve NodePort."
+  exit 1
+fi
+
+echo "Setting up a port-forwarding proxy from 8443 to the gateway NodePort..."
+# Run socat with the retrieved NodePort
+socat TCP-LISTEN:8443,fork TCP:choreo-worker:$NODEPORT &
+
 echo ""
 echo ">>>> Everything prepared, ready to deploy application."
