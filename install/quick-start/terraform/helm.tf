811ff5ef55ae3630fc8b016a8ea3539370179d6c
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-03-21 22:33:17 +0530

    Improve quick-start terraform scripts

--- install/quick-start/terraform/helm.tf
+++ install/quick-start/terraform/helm.tf
@@ -7,6 +7,15 @@ resource "helm_release" "cilium" {
   version         = var.cilium_version
   timeout         = 1800 # 30 minutes
   depends_on = [kind_cluster.kind_choreo, null_resource.connect_container_to_kind_network]
+
+  set {
+    name  = "waitJob.enabled"
+    value = "false"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "helm_release" "choreo" {
@@ -19,4 +28,8 @@ resource "helm_release" "choreo" {
   wait            = false
   timeout         = 1800 # 30 minutes
   depends_on = [helm_release.cilium, null_resource.connect_container_to_kind_network]
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
