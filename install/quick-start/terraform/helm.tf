ff1e38493e48e1ec45baf551774ad13236c33d9d
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-03-24 14:51:05 +0530

    Remove prevent_destroy from qsg helm

--- install/quick-start/terraform/helm.tf
+++ install/quick-start/terraform/helm.tf
@@ -7,10 +7,6 @@ resource "helm_release" "cilium" {
   version         = var.cilium_version
   timeout         = 1800 # 30 minutes
   depends_on = [kind_cluster.kind_choreo, null_resource.connect_container_to_kind_network]
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "helm_release" "choreo" {
@@ -23,8 +19,4 @@ resource "helm_release" "choreo" {
   wait            = false
   timeout         = 1800 # 30 minutes
   depends_on = [helm_release.cilium, null_resource.connect_container_to_kind_network]
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
