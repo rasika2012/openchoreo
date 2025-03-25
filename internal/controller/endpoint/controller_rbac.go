6573390aeedc4eabe66d453be753472f423813cf
jhivandb <jhivanb@gmail.com>
2025-03-25 13:39:45 +0530

    Add rbac to endpoint controller for security policies

--- internal/controller/endpoint/controller_rbac.go
+++ internal/controller/endpoint/controller_rbac.go
@@ -23,6 +23,7 @@ package endpoint
 // +kubebuilder:rbac:groups=core.choreo.dev,resources=endpoints,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=httproutes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=gateway.envoyproxy.io,resources=securitypolicies,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core.choreo.dev,resources=endpoints/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=core.choreo.dev,resources=endpoints/finalizers,verbs=update
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
