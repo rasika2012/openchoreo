2eab5c13034ac2cd26d3ff2d7053919ed3bdca85
jhivandb <jhivanb@gmail.com>
2025-05-21 14:16:55 +0530

    Update license

--- internal/controller/deployment/integrations/kubernetes/cilium_network_policy_handler_test.go
+++ internal/controller/deployment/integrations/kubernetes/cilium_network_policy_handler_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright Open Choreo Authors
+ * Copyright OpenChoreo Authors
  * SPDX-License-Identifier: Apache-2.0
  * The full text of the Apache license is available in the LICENSE file at
  * the root of the repo.
