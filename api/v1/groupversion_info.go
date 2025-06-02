48c93025ae65d32f59755b391c672dd2134f7675
chalindukodikara <chalindu@wso2.com>
2025-06-02 14:36:10 +0530

    Update license header

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -1,4 +1,4 @@
-// Copyright 2025 OpenChoreo Authors
+// Copyright 2025 The OpenChoreo Authors
 // SPDX-License-Identifier: Apache-2.0
 
 // Package v1 contains API Schema definitions for the core v1 API group.
--- pkg/cli/flags/flags.go
+++ pkg/cli/flags/flags.go
@@ -1,4 +1,4 @@
-// Copyright 2025 OpenChoreo Authors
+// Copyright 2025 The OpenChoreo Authors
 // SPDX-License-Identifier: Apache-2.0
 
 package flags
