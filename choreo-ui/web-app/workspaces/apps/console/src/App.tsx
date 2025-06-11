3fd1b39eca6667ad4409e6bad172a71dcc5d544c
Rasika Maduranga <rasikat77@gmail.com>
2025-06-11 10:29:06 +0530

    Restructure console app layout for improved clarity

--- choreo-ui/web-app/workspaces/apps/console/src/App.tsx
+++ choreo-ui/web-app/workspaces/apps/console/src/App.tsx
@@ -1,46 +1,11 @@
-import {
-  ThemeProvider,
-  Box,
-  MenuHomeIcon,
-  MenuHomeFilledIcon,
-  MenuProjectIcon,
-  MenuProjectFilledIcon,
-} from "@open-choreo/design-system";
-import { MainLayout, type MainMenuItem } from "@open-choreo/common-views";
-import { useState } from "react";
+import { ThemeProvider, Box } from "@open-choreo/design-system";
+import { MainLayout } from "./layouts/MainLayout";
 
-
-const mockMenuItems = [
-  {
-    id: "home",
-    label: "Home",
-    icon: <MenuHomeIcon fontSize="inherit" />,
-    filledIcon: <MenuHomeFilledIcon fontSize="inherit" />,
-    path: "/home",
-  },
-  {
-    id: "projects",
-    label: "Projects",
-    icon: <MenuProjectIcon fontSize="inherit" />,
-    filledIcon: <MenuProjectFilledIcon fontSize="inherit" />,
-    path: "/projects",
-  },
-];
 export default function App() {
-  const [selectedMenuItem, setSelectedMenuItem] = useState<MainMenuItem>(
-    mockMenuItems[0],
-  );
   return (
     <ThemeProvider mode="light">
       <Box width="100vw" height="100vh">
-        <MainLayout
-          footer={<Box>Footer</Box>}
-          header={<Box>Header</Box>}
-          menuItems={mockMenuItems}
-          rightSidebar={<Box>Right Sidebar</Box>}
-          selectedMenuItem={selectedMenuItem}
-          onMenuItemClick={setSelectedMenuItem}
-        >
+        <MainLayout>
           <Box>
             Content will be here
           </Box>
