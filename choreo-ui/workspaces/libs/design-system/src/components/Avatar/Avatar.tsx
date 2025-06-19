1a0a11ef6e37089825e7e9744249022c72b65d0f
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-06-19 08:49:22 +0530

    Add AvatarUserName component with styled implementation and storybook integration

--- choreo-ui/workspaces/libs/design-system/src/components/Avatar/Avatar.tsx
+++ choreo-ui/workspaces/libs/design-system/src/components/Avatar/Avatar.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
-import { AvatarProps, StyledAvatar } from './Avatar.styled';
+import { StyledAvatar } from './Avatar.styled';
+
+export type colorVariant =
+  | 'primary'
+  | 'secondary'
+  | 'error'
+  | 'warning'
+  | 'info'
+  | 'success';
+export type avatarVariant = 'circular' | 'rounded' | 'square';
+export type avatarBackgroundColorVariant =
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'error'
+  | 'warning'
+  | 'info'
+  | 'success';
+
+export interface AvatarProps {
+  /**
+   * The content of the component
+   */
+  children?: React.ReactNode;
+  /**
+   * Additional className for the component
+   */
+  className?: string;
+  /**
+   * color variant of the avatar
+   */
+  color?: colorVariant;
+  /**
+   * Optional click handler
+   */
+  onClick?: (event: React.MouseEvent) => void;
+  /**
+   * If true, the component will be disabled
+   */
+  disabled?: boolean;
+  /**
+   * width of the avatar
+   */
+  width?: string | number;
+  /**
+   * height of the avatar
+   * */
+  height?: string | number;
+  /**
+   * The variant of the component
+   */
+  variant?: avatarVariant;
+  /**
+   * The sx prop for custom styles
+   */
+  sx?: React.CSSProperties;
+  /**
+   * Additional props for MUI Avatar
+   */
+  [key: string]: any;
+}
 
 export const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(
   ({ children, ...props }, ref) => {
