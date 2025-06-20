5c2fc8627ee093b680a2c6c2fa80466f3d393622
Rasika Maduranga <rasikat77@gmail.com>
2025-06-20 13:30:47 +0530

    Added overview plugin with dummy nav bar items

--- choreo-ui/workspaces/libs/plugins/plugins/loader.ts
+++ choreo-ui/workspaces/libs/plugins/plugins/loader.ts
@@ -3,5 +3,5 @@
 
 export async function loadPlugin(pluginName: string) {
     // Assumes each plugin has an index.ts exporting the manifest
-    return import(`./${pluginName}`);
+    return (await import(`./${pluginName}`)).default;
 } 
