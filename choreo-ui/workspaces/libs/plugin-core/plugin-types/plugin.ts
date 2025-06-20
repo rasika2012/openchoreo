068c056c3ee2fba7c9a8b6db4cbbf1e00542bb07
Rasika Maduranga <rasikat77@gmail.com>
2025-06-20 14:03:20 +0530

    Refactor: update package.json for plugin configuration. setup hot build command on plugins project

--- choreo-ui/workspaces/libs/plugin-core/plugin-types/plugin.ts
+++ choreo-ui/workspaces/libs/plugin-core/plugin-types/plugin.ts
@@ -1,35 +1,35 @@
 import { type ComponentType, type LazyExoticComponent } from "react";
 
-export enum PluginEntryType {
+export enum PluginExtensionType {
     NAVIGATION = "nav-item",
     PAGE = "page"
 }
 export interface PluginManifest  {
     name: string;
     description: string;
-    entries: PluginEntry[];
+    extensions: PluginExtension[];
 }
 
-interface PluginEntrySubmenu {
+interface PluginExtensionSubmenu {
     name: string;
     icon: ComponentType<{className?: string}> | LazyExoticComponent<ComponentType<{className?: string}>>;
     iconSelected: ComponentType<{className?: string}> | LazyExoticComponent<ComponentType<{className?: string}>>;
     path?: string;
 }
 
-interface PluginEntryNavigation  {
-    type: PluginEntryType.NAVIGATION;
+interface PluginExtensionNavigation  {
+    type: PluginExtensionType.NAVIGATION;
     name: string;
     icon: ComponentType<{className?: string}> | LazyExoticComponent<ComponentType<{className?: string}>>;
     iconSelected: ComponentType<{className?: string}> | LazyExoticComponent<ComponentType<{className?: string}>>;
     path?: string;
-    submenu?: PluginEntrySubmenu[];
+    submenu?: PluginExtensionSubmenu[];
 }
 
-interface PluginEntryPage {
-    type: PluginEntryType.PAGE;
+interface PluginExtensionPage {
+    type: PluginExtensionType.PAGE;
     path: string;
     component: ComponentType<{}> | LazyExoticComponent<ComponentType<{}>>;
 }
 
-export type PluginEntry = PluginEntryNavigation | PluginEntryPage;
+export type PluginExtension = PluginExtensionNavigation | PluginExtensionPage;
