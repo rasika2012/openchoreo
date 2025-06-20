aeba1ea923eb91ac957425c5a7ad4b4c758f925f
Rasika Maduranga <rasikat77@gmail.com>
2025-06-20 14:03:20 +0530

    Refactor: update package.json for plugin configuration. setup hot build command on plugins project

--- choreo-ui/workspaces/libs/plugins/plugins/loader.ts
+++
@@ -1,7 +0,0 @@
-// Plugin loader utility for dynamic imports
-// Usage: const plugin = await loadPlugin('overview');
-
-export async function loadPlugin(pluginName: string) {
-    // Assumes each plugin has an index.ts exporting the manifest
-    return (await import(`./${pluginName}`)).default;
-} 
--- choreo-ui/workspaces/plugins/overview/src/pages/index.tsx
+++ choreo-ui/workspaces/plugins/overview/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { type PluginEntry, PluginEntryType } from "../../../types";
+import { type PluginExtension, PluginExtensionType } from "@open-choreo/plugin-core";
 import React from "react";
 const OverviewPage = React.lazy(() => import("./OverviewPage"));
 // import OverviewPage from "./OverviewPage";
 
-export const page: PluginEntry = {
-    type: PluginEntryType.PAGE,
+export const page: PluginExtension = {
+    type: PluginExtensionType.PAGE,
     path: "/overview",
     component: OverviewPage,
 };
