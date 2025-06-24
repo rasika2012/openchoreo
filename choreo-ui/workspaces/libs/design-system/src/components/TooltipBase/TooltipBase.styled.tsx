c9eaad76ab379885a49f52cb3406af18d7c4b2fa
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-06-24 14:34:34 +0530

    Refactor Tooltip and TooltipBase components to remove disabled prop, update stories, and enhance tooltip rendering with improved structure and styling

--- choreo-ui/workspaces/libs/design-system/src/components/TooltipBase/TooltipBase.styled.tsx
+++ choreo-ui/workspaces/libs/design-system/src/components/TooltipBase/TooltipBase.styled.tsx
@@ -1,36 +1,31 @@
 import { styled, Tooltip, TooltipProps } from '@mui/material';
 import { ComponentType } from 'react';
 
-export interface StyledTooltipBaseProps extends TooltipProps {
-  disabled?: boolean;
-}
+export interface StyledTooltipBaseProps extends TooltipProps {}
 
 export const StyledTooltipBase: ComponentType<StyledTooltipBaseProps> = styled(
   Tooltip,
   { shouldForwardProp: (prop) => !['disabled'].includes(prop as string) }
-)<StyledTooltipBaseProps>(({ disabled, theme }) => ({
-  opacity: disabled ? 0.5 : 1,
-  cursor: disabled ? 'not-allowed' : 'pointer',
-  backgroundColor: 'transparent',
-  pointerEvents: disabled ? 'none' : 'auto',
-  '&:hover': {
-    backgroundColor: theme.palette.action.hover,
+)<StyledTooltipBaseProps>(({ theme }) => ({
+  '& .MuiTooltip-tooltip': {
+    '&.infoTooltipDark': {
+      color: theme.palette.grey[300],
+      backgroundColor: theme.palette.secondary.dark,
+      borderRadius: 5,
+    },
+    '&.infoTooltipLight': {
+      color: theme.palette.secondary.dark,
+      backgroundColor: theme.palette.common.white,
+      borderRadius: 5,
+      maxWidth: theme.spacing(53),
+    },
   },
-  '.infoTooltipDark': {
-    color: theme.palette.grey[100],
-    backgroundColor: theme.palette.secondary.dark,
-    borderRadius: 5,
-  },
-  '.infoArrowDark': {
-    color: theme.palette.secondary.dark,
-  },
-  '.infoTooltipLight': {
-    color: theme.palette.secondary.dark,
-    backgroundColor: theme.palette.common.white,
-    borderRadius: 5,
-    maxWidth: theme.spacing(53),
-  },
-  '.infoArrowLight': {
-    color: theme.palette.common.white,
+  '& .MuiTooltip-arrow': {
+    '&.infoArrowDark': {
+      color: theme.palette.secondary.dark,
+    },
+    '&.infoArrowLight': {
+      color: theme.palette.common.white,
+    },
   },
 }));
