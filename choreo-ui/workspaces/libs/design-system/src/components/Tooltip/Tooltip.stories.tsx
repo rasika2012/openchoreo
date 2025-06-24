aac64c86617f0985065b02e379b4bff67d8ab106
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-06-24 14:34:34 +0530

    Refactor Tooltip and TooltipBase components to remove disabled prop, update stories, and enhance tooltip rendering with improved structure and styling

--- choreo-ui/workspaces/libs/design-system/src/components/Tooltip/Tooltip.stories.tsx
+++ choreo-ui/workspaces/libs/design-system/src/components/Tooltip/Tooltip.stories.tsx
@@ -3,20 +3,15 @@ import { Tooltip } from './Tooltip';
 import { Button } from '../Button';
 import { QuestionMark } from '@mui/icons-material';
 import { Box, Typography } from '@mui/material';
+import { Card, CardContent } from '../Card';
+import Question from '@design-system/Icons/generated/Question';
+import Info from '@design-system/Icons/generated/Info';
 
 const meta: Meta<typeof Tooltip> = {
   title: 'Components/Tooltip',
   component: Tooltip,
   tags: ['autodocs'],
   argTypes: {
-    disabled: {
-      control: 'boolean',
-      description: 'Disables the element',
-      table: {
-        type: { summary: 'boolean' },
-        defaultValue: { summary: 'false' },
-      },
-    },
     onClick: {
       action: 'clicked',
       description: 'Click event handler',
@@ -42,22 +37,6 @@ export const Default: Story = {
   ),
 };
 
-export const Disabled: Story = {
-  args: {
-    children: 'Disabled Tooltip',
-    disabled: true,
-  },
-  render: (args) => {
-    return (
-      <Tooltip {...args}>
-        <Button variant="contained" color="primary">
-          Hover me
-        </Button>
-      </Tooltip>
-    );
-  },
-};
-
 export const WithIcon: Story = {
   args: {
     children: 'Tooltip with icon',
@@ -72,44 +51,47 @@ export const WithIcon: Story = {
   },
 };
 
-export const ToNormalText: Story = {
-  args: {
-    children: 'Tooltip to normal text',
-    title: 'Tooltip to normal text',
-  },
-  render: (args) => {
+export const primary: Story = {
+  render: () => {
     return (
-      <Tooltip {...args}>
-        <span>Hover over here</span>
-      </Tooltip>
-    );
-  },
-};
-
-export const WithHeadingAndContent: Story = {
-  args: {
-    children: 'Tooltip with heading and content',
-    title: 'Tooltip Heading',
-    content: 'This is the content of the tooltip.',
-  },
-  render: (args) => {
-    return (
-      <Tooltip
-        {...args}
-        title={
-          <Box>
-            <Typography variant="h4">Title</Typography>
-            <Typography variant="body1">
-              Create programs that trigger via events. E.g., Business automation
-              tasks.
-            </Typography>
+      <Card testId="tooltip">
+        <CardContent>
+          <Box mb={3}>
+            <Tooltip title="This is a create Button">
+              <Button testId="tooltip-action-1-button">Create Tooltip</Button>
+            </Tooltip>
           </Box>
-        }
-      >
-        <Button variant="contained" color="primary">
-          Hover me
-        </Button>
-      </Tooltip>
+          <Box mb={3}>
+            <Tooltip title="This is a info icon" darken>
+              <Question />
+            </Tooltip>
+          </Box>
+          <Box mb={3}>
+            <Tooltip
+              title={
+                <Box>
+                  <Typography variant="h4">Title</Typography>
+                  <Typography variant="body1">
+                    Create programs that trigger via events. E.g., Business
+                    automation tasks.
+                  </Typography>
+                </Box>
+              }
+            >
+              <Info />
+            </Tooltip>
+          </Box>
+          <Box mb={3}>
+            <Tooltip
+              heading="heading"
+              example="1,2,3"
+              content="Tooltip content goes here"
+            >
+              <Button testId="tooltip-action-3-button">Custom Content</Button>
+            </Tooltip>
+          </Box>
+        </CardContent>
+      </Card>
     );
   },
 };
--- choreo-ui/workspaces/libs/design-system/src/components/TooltipBase/TooltipBase.stories.tsx
+++ choreo-ui/workspaces/libs/design-system/src/components/TooltipBase/TooltipBase.stories.tsx
@@ -6,14 +6,6 @@ const meta: Meta<typeof TooltipBase> = {
   component: TooltipBase,
   tags: ['autodocs'],
   argTypes: {
-    disabled: {
-      control: 'boolean',
-      description: 'Disables the element',
-      table: {
-        type: { summary: 'boolean' },
-        defaultValue: { summary: 'false' },
-      },
-    },
     onClick: {
       action: 'clicked',
       description: 'Click event handler',
@@ -27,12 +19,5 @@ type Story = StoryObj<typeof TooltipBase>;
 export const Default: Story = {
   args: {
     children: 'TooltipBase Content',
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    children: 'Disabled TooltipBase',
-    disabled: true,
   },
 };
