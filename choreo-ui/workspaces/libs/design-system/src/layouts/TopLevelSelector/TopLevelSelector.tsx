e54b89ed81b0f56c15e40dff733e3059bb1d9e51
Rasika Maduranga <rasikat77@gmail.com>
2025-06-25 15:16:44 +0530

    Refactor plugin panel - Added panel plugin extention points loading component - Added top level plugins panel loader - Implimented mocked top level selector as a separate plugin

--- choreo-ui/workspaces/libs/design-system/src/layouts/TopLevelSelector/TopLevelSelector.tsx
+++ choreo-ui/workspaces/libs/design-system/src/layouts/TopLevelSelector/TopLevelSelector.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { StyledPopover, StyledTopLevelSelector } from './TopLevelSelector.styled';
 import { Box, Divider, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
-import { IconButton } from '../IconButton';
 import { AddIcon, ChevronDownIcon, CloseIcon, SearchIcon } from '@design-system/Icons';
-import { TextInput } from '../TextInput';
-import { Button } from '../Button';
+import { TextInput } from '../../components/TextInput';
+import { Button } from '../../components/Button';
+import { IconButton } from '@design-system/components';
 
 export enum Level {
   ORGANIZATION = 'organization',
@@ -48,7 +48,6 @@ const getLevelLabel = (level: Level) => {
 export const TopLevelSelector = React.forwardRef<HTMLDivElement, TopLevelSelectorProps>(
   ({ items, selectedItem, onSelect, isHighlighted, disabled, onClick, level, recentItems = [], onClose }, ref) => {
     const [search, setSearch] = useState('');
-
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
     const open = Boolean(anchorEl);
 
@@ -80,23 +79,24 @@ export const TopLevelSelector = React.forwardRef<HTMLDivElement, TopLevelSelecto
         disabled={disabled}
         variant="outlined"
         isHighlighted={isHighlighted}
-
       >
-        <Box display="flex" alignItems="center" justifyContent="space-between" gap={1} flexGrow={1}>
-          <Typography variant="body2" color="text.secondary">
-            {getLevelLabel(level)}
-          </Typography>
-          <IconButton size="small" variant="text" onClick={handleClose}>
-            <CloseIcon fontSize="inherit" />
-          </IconButton>
-        </Box>
-        <Box display="flex" alignItems="center" gap={1}>
-          <Typography variant="body1" color="text.primary">
-            Sample Project
-          </Typography>
-          <IconButton size="small" variant="text" onClick={handleOpen}>
-            <ChevronDownIcon />
-          </IconButton>
+        <Box display="flex" flexDirection="column">
+          <Box display="flex" alignItems="center" justifyContent="space-between" flexGrow={1}>
+            <Typography variant="body2" fontSize={11} color="text.secondary">
+              {getLevelLabel(level)}
+            </Typography>
+            <IconButton size="tiny" color="secondary" disableRipple onClick={handleClose}>
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          </Box>
+          <Box display="flex" alignItems="center" gap={1} marginRight={5}>
+            <Typography variant="body1" fontSize={14} fontWeight={450} color="text.primary">
+              Sample Project
+            </Typography>
+            <IconButton size="tiny" disableRipple onClick={handleOpen}>
+              <ChevronDownIcon />
+            </IconButton>
+          </Box>
         </Box>
         <StyledPopover
           id={`${level}-popover`}
