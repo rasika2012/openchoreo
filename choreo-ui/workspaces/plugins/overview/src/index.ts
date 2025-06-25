d8409b116f93b884e6109085dd84e52e215885c2
Rasika Maduranga <rasikat77@gmail.com>
2025-06-25 09:26:49 +0530

    Add plugin extention mounter hooks - Page Extentions - NavBar Extentions

--- choreo-ui/workspaces/plugins/overview/src/index.ts
+++ choreo-ui/workspaces/plugins/overview/src/index.ts
@@ -2,10 +2,9 @@ import { type PluginManifest } from "@open-choreo/plugin-core";
 
 import {navigation} from "./nav-items";
 import {page} from "./pages";
-import {navigation as navigationsOthers} from "./nav-items-others";
 
 export const overviewPlugin = {
     name: "Overview",
     description: "Overview plugin",
-    extensions: [navigation, page, navigationsOthers],
+    extensions: [navigation, page],
 } as PluginManifest;
