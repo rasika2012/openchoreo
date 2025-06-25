a8ef780aabfe8c5cc404438ba5ba4315b83e3a93
Rasika Maduranga <rasikat77@gmail.com>
2025-06-25 09:26:49 +0530

    Add plugin extention mounter hooks - Page Extentions - NavBar Extentions

--- choreo-ui/workspaces/apps/console/src/App.tsx
+++ choreo-ui/workspaces/apps/console/src/App.tsx
@@ -1,16 +1,11 @@
 import { ThemeProvider, Box } from "@open-choreo/design-system";
 import { MainLayout } from "./layouts/MainLayout";
-import { Route, Routes } from "react-router";
 import { useEffect } from "react";
 import { pluginRegistry } from "./plugins";
-import { PluginExtensionType } from "@open-choreo/plugin-core";
-
-const pages = pluginRegistry.flatMap(plugin => plugin.extensions.filter(entry => entry.type === PluginExtensionType.PAGE).map(entry => ({
-  path: entry.path,
-  element: <entry.component />,
-})));
+import { useMainPagePlgins } from "@open-choreo/plugin-core";
 
 export default function App() {
+  const pages = useMainPagePlgins(pluginRegistry);
   useEffect(() => {
     console.log(pages, "pages");
   }, []);
@@ -18,13 +13,7 @@ export default function App() {
     <ThemeProvider mode="light">
       <Box width="100vw" height="100vh">
         <MainLayout>
-          <Routes>
-            {
-              pages.map((page) => (
-                <Route key={page.path} path={page.path + "/:subpath"} element={page.element} />
-              ))
-            }
-          </Routes>
+          {pages}
         </MainLayout>
       </Box>
     </ThemeProvider>
