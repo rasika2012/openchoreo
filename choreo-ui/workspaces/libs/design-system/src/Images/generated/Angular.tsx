114566872403a0c28e3495682fdb11a999543a20
Rasika Maduranga <rasikat77@gmail.com>
2025-06-26 17:16:06 +0530

    Add Images (svg's) to ds

---
+++ choreo-ui/workspaces/libs/design-system/src/Images/generated/Angular.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+
+import * as React from 'react';
+
+
+export default function (props: React.SVGProps<SVGSVGElement>){
+  return (
+    <svg {...props}>
+      <g><React.Fragment><path fill="#DD0031" d="M24.995 0 1.72 8.3l3.55 30.775L24.995 50 44.72 39.075 48.27 8.3z" /><path fill="#C3002F" d="M24.995 0v5.55-.025V50L44.72 39.075 48.27 8.3z" /><path fill="white" d="M24.995 5.525 10.445 38.15h5.425l2.925-7.3h12.35l2.925 7.3h5.425zm4.25 20.825h-8.5l4.25-10.225z" /></React.Fragment></g>
+    </svg>
+  )
+}
+    
+
---
+++ choreo-ui/workspaces/libs/design-system/src/Images/generated/CopyIcon.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+
+import * as React from 'react';
+
+
+export default function (props: React.SVGProps<SVGSVGElement>){
+  return (
+    <svg {...props}>
+      <g><React.Fragment><defs></defs><g data-name="Icon/12/Plus"><g data-name="Group 80"><path fill="#cbcedb" d="M2 12a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2Zm0-2h5V5H2Zm8.007-1.884L10 8V3a1 1 0 0 0-.883-.993L9 2H5A1 1 0 0 1 4.883.006L5 0h4a3 3 0 0 1 3 2.823V8a1 1 0 0 1-1.993.116" data-name="Combined Shape" transform="translate(10 10)" /><g data-name="Mask Group 80" transform="translate(10 10)"><path fill="#8c91a3" d="M0 0H12V12H0z" /></g></g></g></React.Fragment></g>
+    </svg>
+  )
+}
+    
+
