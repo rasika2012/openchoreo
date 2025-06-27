ea79affde7b669a1ab8abdc86eab968e35633cd6
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-06-27 10:28:18 +0530

    Refactor SVG components and styles for improved consistency and readability
    - Updated SVG components to use camelCase for SVG attributes (e.g., stroke-linecap to strokeLinecap).
    - Removed unnecessary ESLint disable comments in SVG files.
    - Cleaned up imports and organized styled components in various files for better maintainability.
    - Enhanced the TopLevelSelector component with improved formatting and structure.
    - Ensured consistent export statements across index files.

--- choreo-ui/workspaces/libs/design-system/src/Images/generated/InfoBlue.tsx
+++ choreo-ui/workspaces/libs/design-system/src/Images/generated/InfoBlue.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable max-len */
+ 
 
 import * as React from 'react';
 
@@ -7,7 +7,7 @@ import * as React from 'react';
 export default function (props: React.SVGProps<SVGSVGElement>){
   return (
     <svg {...props}>
-      <g><React.Fragment><g fill="none" fillRule="evenodd" stroke="none" stroke-width="1" transform="translate(-89 -736) translate(89 736)"><circle cx="8" cy="8" r="8" fill="#5567D5" fillRule="nonzero" /><path fill="#FFFFFF" d="M8 6c.5523 0 1 .4477 1 1v4c0 .5523-.4477 1-1 1s-1-.4477-1-1V7c0-.5523.4477-1 1-1m0-3c.5523 0 1 .4477 1 1s-.4477 1-1 1-1-.4477-1-1 .4477-1 1-1" /></g></React.Fragment></g>
+      <g><React.Fragment><g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1" transform="translate(-89 -736) translate(89 736)"><circle cx="8" cy="8" r="8" fill="#5567D5" fillRule="nonzero" /><path fill="#FFFFFF" d="M8 6c.5523 0 1 .4477 1 1v4c0 .5523-.4477 1-1 1s-1-.4477-1-1V7c0-.5523.4477-1 1-1m0-3c.5523 0 1 .4477 1 1s-.4477 1-1 1-1-.4477-1-1 .4477-1 1-1" /></g></React.Fragment></g>
     </svg>
   )
 }
