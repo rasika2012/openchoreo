5df5af2306c9226ed464611c46b130cf4ff0ab03
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-06-27 10:28:18 +0530

    Refactor SVG components and styles for improved consistency and readability
    - Updated SVG components to use camelCase for SVG attributes (e.g., stroke-linecap to strokeLinecap).
    - Removed unnecessary ESLint disable comments in SVG files.
    - Cleaned up imports and organized styled components in various files for better maintainability.
    - Enhanced the TopLevelSelector component with improved formatting and structure.
    - Ensured consistent export statements across index files.

--- choreo-ui/workspaces/libs/design-system/src/layouts/TopLevelSelector/TopLevelSelector.styled.tsx
+++ choreo-ui/workspaces/libs/design-system/src/layouts/TopLevelSelector/TopLevelSelector.styled.tsx
@@ -1,37 +1,54 @@
-import { alpha, Card, CardProps, Popover, PopoverProps, styled } from "@mui/material";
-import { ComponentType } from "react";
-
+import {
+  alpha,
+  Card,
+  CardProps,
+  Popover,
+  PopoverProps,
+  styled,
+} from '@mui/material';
+import { ComponentType } from 'react';
 
 export interface StyledTopLevelSelectorProps {
   disabled?: boolean;
   isHighlighted?: boolean;
 }
 
-export const StyledTopLevelSelector: ComponentType<StyledTopLevelSelectorProps & CardProps> = styled(Card)<CardProps & StyledTopLevelSelectorProps>(({ disabled, theme, isHighlighted }) => ({
-  opacity: disabled ? 0.5 : 1,
-  cursor: disabled ? 'not-allowed' : 'pointer',
-  pointerEvents: disabled ? 'none' : 'auto',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'space-between',
-  justifyContent: 'center',
-  height: theme.spacing(5),
-  gap: theme.spacing(1),
-  backgroundColor: isHighlighted ? alpha(theme.palette.primary.light, 0.05) : 'transparent',
-  borderColor: isHighlighted ? theme.palette.primary.main : theme.palette.divider,
-  transition: theme.transitions.create(['background-color'], {
-    duration: theme.transitions.duration.short,
-  }),
-  '&:hover': {
-    backgroundColor: isHighlighted ? alpha(theme.palette.primary.light, 0.15) : theme.palette.action.hover,
-  },
-  padding: theme.spacing(0.615),
+export const StyledTopLevelSelector: ComponentType<
+  StyledTopLevelSelectorProps & CardProps
+> = styled(Card)<CardProps & StyledTopLevelSelectorProps>(
+  ({ disabled, theme, isHighlighted }) => ({
+    opacity: disabled ? 0.5 : 1,
+    cursor: disabled ? 'not-allowed' : 'pointer',
+    pointerEvents: disabled ? 'none' : 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'space-between',
+    justifyContent: 'center',
+    height: theme.spacing(5),
+    gap: theme.spacing(1),
+    backgroundColor: isHighlighted
+      ? alpha(theme.palette.primary.light, 0.05)
+      : 'transparent',
+    borderColor: isHighlighted
+      ? theme.palette.primary.main
+      : theme.palette.divider,
+    transition: theme.transitions.create(['background-color'], {
+      duration: theme.transitions.duration.short,
+    }),
+    '&:hover': {
+      backgroundColor: isHighlighted
+        ? alpha(theme.palette.primary.light, 0.15)
+        : theme.palette.action.hover,
+    },
+    padding: theme.spacing(0.615),
+  })
+);
 
-}));
-
-export const StyledPopover: ComponentType<PopoverProps> = styled(Popover)(({ theme }) => ({
-  '& .MuiPopover-paper': {
-    boxShadow: theme.shadows[1],
-    width: theme.spacing(40),
-  },
-}));
+export const StyledPopover: ComponentType<PopoverProps> = styled(Popover)(
+  ({ theme }) => ({
+    '& .MuiPopover-paper': {
+      boxShadow: theme.shadows[1],
+      width: theme.spacing(40),
+    },
+  })
+);
