ccfccc65a925e419388fafad7202bc2d73df7673
Rasika Maduranga <rasikat77@gmail.com>
2025-06-30 13:05:23 +0530

    Add intl to project - Installes i18n dependancy - setup en.json genaration scripts - Add intl to error pages as a initial step

--- choreo-ui/workspaces/libs/views/src/views/MainLayout/MainLayout.tsx
+++ choreo-ui/workspaces/libs/views/src/views/MainLayout/MainLayout.tsx
@@ -53,7 +53,6 @@ export const MainLayout = React.forwardRef<HTMLDivElement, MainLayoutProps>(
           <ImageChoreo height={30} width={140} />
           {header}
         </Header>
-
         <Box flexGrow={1} flexDirection="row" display="flex" overflow="hidden">
           {menuItems && (
             <Sidebar
