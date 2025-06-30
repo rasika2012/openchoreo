b942c0e86c5c51cc457af885821b9c38a4284a99
Rasika Maduranga <rasikat77@gmail.com>
2025-06-30 11:33:02 +0530

    Enhance plugin configurations and improve code formatting
    - Added lint scripts to package.json files for better code quality checks.
    - Updated ESLint configurations to ignore the 'dist' directory across multiple plugins.

--- choreo-ui/workspaces/plugins/top-level-selector/src/index.ts
+++ choreo-ui/workspaces/plugins/top-level-selector/src/index.ts
@@ -1,9 +1,9 @@
 import { type PluginManifest } from "@open-choreo/plugin-core";
 
-import {panel} from "./panel";
+import { panel } from "./panel";
 
 export const levelSelectorPlugin = {
-    name: "Top Level Selector",
-    description: "Top Level Selector",
-    extensions: [panel],
+  name: "Top Level Selector",
+  description: "Top Level Selector",
+  extensions: [panel],
 } as PluginManifest;
--- choreo-ui/workspaces/plugins/top-level-selector/src/panel/TopLevelSelector.tsx
+++ choreo-ui/workspaces/plugins/top-level-selector/src/panel/TopLevelSelector.tsx
@@ -1,15 +1,44 @@
-import { Box, Level, TopLevelSelector, Typography, useChoreoTheme } from "@open-choreo/design-system";
-import React from "react";
+import {
+    Box,
+    Level,
+    TopLevelSelector,
+    useChoreoTheme,
+} from "@open-choreo/design-system";
 
 const Panel: React.FC = () => {
     const theme = useChoreoTheme();
     return (
-        <Box display="flex" flexDirection="row" gap={theme.spacing(1)} padding={theme.spacing(0, 2)} alignItems="center" height="100%" >
-            <TopLevelSelector items={[]} recentItems={[]} selectedItem={{ label: "Overview", id: "overview" }} level={Level.ORGANIZATION} onSelect={() => { }} />
-            <TopLevelSelector items={[]} recentItems={[]} selectedItem={{ label: "Overview", id: "overview" }} level={Level.PROJECT} onSelect={() => { }} />
-            <TopLevelSelector items={[]} recentItems={[]} selectedItem={{ label: "Overview", id: "overview" }} level={Level.COMPONENT} onSelect={() => { }} />
+        <Box
+            display="flex"
+            flexDirection="row"
+            gap={theme.spacing(1)}
+            padding={theme.spacing(0, 2)}
+            alignItems="center"
+            height="100%"
+        >
+            <TopLevelSelector
+                items={[]}
+                recentItems={[]}
+                selectedItem={{ label: "Overview", id: "overview" }}
+                level={Level.ORGANIZATION}
+                onSelect={() => { }}
+            />
+            <TopLevelSelector
+                items={[]}
+                recentItems={[]}
+                selectedItem={{ label: "Overview", id: "overview" }}
+                level={Level.PROJECT}
+                onSelect={() => { }}
+            />
+            <TopLevelSelector
+                items={[]}
+                recentItems={[]}
+                selectedItem={{ label: "Overview", id: "overview" }}
+                level={Level.COMPONENT}
+                onSelect={() => { }}
+            />
         </Box>
     );
 };
 
-export default Panel; 
+export default Panel;
