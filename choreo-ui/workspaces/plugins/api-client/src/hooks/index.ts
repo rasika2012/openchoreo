de9bb55c394852ad25ae0f5d5c53db0275b24e7c
Rasika Maduranga <rasikat77@gmail.com>
2025-06-30 15:07:05 +0530

    Improved API client functionality - Added new hooks for managing project and component states. - Integrated global state management using context for better state handling. - Modified the OverviewPage to utilize global state for project data.

--- choreo-ui/workspaces/plugins/api-client/src/hooks/index.ts
+++ choreo-ui/workspaces/plugins/api-client/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./useComponent";
 export * from "./useClient";
+export * from "./useGlobleState";
--- choreo-ui/workspaces/plugins/api-client/src/providers/ApiClientProvider.tsx
+++ choreo-ui/workspaces/plugins/api-client/src/providers/ApiClientProvider.tsx
@@ -1,50 +1,57 @@
 import React, {
-  createContext,
-  Dispatch,
-  ReactNode,
-  useMemo,
-  useReducer,
+    createContext,
+    Dispatch,
+    ReactNode,
+    useMemo,
+    useReducer,
 } from "react";
 import {
-  appStateReducer,
-  IAppState,
-  IAppStateAction,
-  initialState,
+    appStateReducer,
+    IAppState,
+    IAppStateAction,
+    initialState,
 } from "../reducers/appState";
 import { ChoreoClient } from "@open-choreo/api-client-lib";
 import { useBasePath } from "@open-choreo/plugin-core";
+import { GlobalStateProvider } from "./GlobleStateProvider";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export interface ApiClientProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 export interface IApiClientContext {
-  state: IAppState;
-  dispatch: Dispatch<IAppStateAction>;
-  apiClient: ChoreoClient;
+    state: IAppState;
+    dispatch: Dispatch<IAppStateAction>;
+    apiClient: ChoreoClient;
 }
 
 export const ApiClientContext = createContext<IApiClientContext>({
-  state: initialState,
-  dispatch: () => {},
-  apiClient: new ChoreoClient(),
+    state: initialState,
+    dispatch: () => { },
+    apiClient: new ChoreoClient(),
 });
 
 const ApiClientPanel: React.FC<ApiClientProviderProps> = (
-  props: ApiClientProviderProps,
+    props: ApiClientProviderProps,
 ) => {
-  const basePath = useBasePath();
-  const apiClient = useMemo(
-    () => new ChoreoClient({ baseUrl: basePath }),
-    [basePath],
-  );
-  const [state, dispatch] = useReducer(appStateReducer, initialState);
+    const basePath = useBasePath();
+    const apiClient = useMemo(
+        () => new ChoreoClient({ baseUrl: basePath }),
+        [basePath],
+    );
+    const queryClient = useMemo(() => new QueryClient(), []);
+    const [state, dispatch] = useReducer(appStateReducer, initialState);
 
-  return (
-    <ApiClientContext.Provider value={{ apiClient, state, dispatch }}>
-      {props.children}
-    </ApiClientContext.Provider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ApiClientContext.Provider value={{ apiClient, state, dispatch }}>
+                <GlobalStateProvider>
+                    {props.children}
+                </GlobalStateProvider>
+            </ApiClientContext.Provider>
+        </QueryClientProvider>
+    );
 };
 
 export default ApiClientPanel;
