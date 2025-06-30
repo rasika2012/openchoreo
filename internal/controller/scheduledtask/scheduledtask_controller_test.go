57d0bb22cab4a3923bb9266c51a44d25b95dc36e
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-06-30 16:06:44 +0530

    Fix compilation errors and lint issues

--- internal/controller/scheduledtask/scheduledtask_controller_test.go
+++ internal/controller/scheduledtask/scheduledtask_controller_test.go
@@ -9,13 +9,11 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	corev1 "github.com/openchoreo/openchoreo/api/v1"
-	"github.com/openchoreo/openchoreo/internal/controller"
 )
 
 var _ = Describe("ScheduledTask Controller", func() {
@@ -32,7 +30,7 @@ var _ = Describe("ScheduledTask Controller", func() {
 
 		BeforeEach(func() {
 			By("creating the custom resource for the Kind ScheduledTask")
-			err := controller.k8sClient.Get(ctx, typeNamespacedName, scheduledtask)
+			err := k8sClient.Get(ctx, typeNamespacedName, scheduledtask)
 			if err != nil && errors.IsNotFound(err) {
 				resource := &corev1.ScheduledTask{
 					ObjectMeta: metav1.ObjectMeta{
@@ -41,24 +39,24 @@ var _ = Describe("ScheduledTask Controller", func() {
 					},
 					// TODO(user): Specify other spec details if needed.
 				}
-				Expect(controller.k8sClient.Create(ctx, resource)).To(Succeed())
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
 		})
 
 		AfterEach(func() {
 			// TODO(user): Cleanup logic after each test, like removing the resource instance.
 			resource := &corev1.ScheduledTask{}
-			err := controller.k8sClient.Get(ctx, typeNamespacedName, resource)
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
 			Expect(err).NotTo(HaveOccurred())
 
 			By("Cleanup the specific resource instance ScheduledTask")
-			Expect(controller.k8sClient.Delete(ctx, resource)).To(Succeed())
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
 		})
 		It("should successfully reconcile the resource", func() {
 			By("Reconciling the created resource")
 			controllerReconciler := &Reconciler{
-				Client: controller.k8sClient,
-				Scheme: controller.k8sClient.Scheme(),
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
 			}
 
 			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
