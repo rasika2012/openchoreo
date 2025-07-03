4a1ffff2de9fbcc0bcc8240f66a56b7c6cbfc36a
Rasika Maduranga <rasikat77@gmail.com>
2025-07-03 11:42:49 +0530

    Fix route match logics in  left nav bar

--- choreo-ui/workspaces/plugins/overview/src/nav-items/index.tsx
+++ choreo-ui/workspaces/plugins/overview/src/nav-items/index.tsx
@@ -1,13 +1,37 @@
 import {
+    BasePathPatterns,
+    PathsPatterns,
     type PluginExtension,
     PluginExtensionType,
 } from "@open-choreo/plugin-core";
-import { MenuHomeFilledIcon, MenuHomeIcon, MenuObserveFilledIcon, MenuObserveIcon, MenuOverviewFilledIcon, MenuOverviewIcon } from "@open-choreo/design-system";
+import { Level, MenuHomeFilledIcon, MenuHomeIcon, MenuObserveFilledIcon, MenuObserveIcon, MenuOverviewFilledIcon, MenuOverviewIcon } from "@open-choreo/design-system";
 
-export const navigation: PluginExtension = {
+export const projectOverviewNavigation: PluginExtension = {
     type: PluginExtensionType.NAVIGATION,
     icon: () => <MenuHomeIcon fontSize="inherit" />,
     iconSelected: () => <MenuHomeFilledIcon fontSize="inherit" />,
     path: "",
     name: "Overview",
+    pathPattern: BasePathPatterns.PROJECT_LEVEL,
+    extentionPointId: Level.PROJECT,
+};
+
+export const componentOverviewNavigation: PluginExtension = {
+    type: PluginExtensionType.NAVIGATION,
+    icon: () => <MenuHomeIcon fontSize="inherit" />,
+    iconSelected: () => <MenuHomeFilledIcon fontSize="inherit" />,
+    path: "",
+    name: "Overview",
+    pathPattern: BasePathPatterns.COMPONENT_LEVEL,
+    extentionPointId: Level.COMPONENT,
+};
+
+export const orgOverviewNavigation: PluginExtension = {
+    type: PluginExtensionType.NAVIGATION,
+    icon: () => <MenuHomeIcon fontSize="inherit" />,
+    iconSelected: () => <MenuHomeFilledIcon fontSize="inherit" />,
+    path: "",
+    name: "Overview",
+    pathPattern: BasePathPatterns.ORG_LEVEL,
+    extentionPointId: Level.ORGANIZATION,
 };
