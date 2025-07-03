78e08bbac5d9f8f1dafe1b94699596222d42bc66
Rasika Maduranga <rasikat77@gmail.com>
2025-07-03 17:02:57 +0530

    Update dependencies and improve code structure across multiple plugins - Adjusted build scripts in plugin `package.json` files to run lint. - Improved code formatting and consistency across several files.

--- choreo-ui/workspaces/plugins/api-client/src/providers/GlobleStateProvider.tsx
+++ choreo-ui/workspaces/plugins/api-client/src/providers/GlobleStateProvider.tsx
@@ -1,36 +1,52 @@
 import React, { createContext } from "react";
 import { useComponent, useComponentList } from "../hooks";
 import { UseQueryResult } from "@tanstack/react-query";
-import { Component, ComponentList, Project, ProjectList } from "@open-choreo/api-client-lib";
+import {
+  Component,
+  ComponentList,
+  Project,
+  ProjectList,
+} from "@open-choreo/api-client-lib";
 import { useProject, useProjectList } from "../hooks/useProjects";
 import { useComponentHandle, useProjectHandle } from "@open-choreo/plugin-core";
 
 export interface GlobalState {
-    componentQueryResult: UseQueryResult<Component, Error> | null;
-    componentListQueryResult: UseQueryResult<ComponentList, Error> | null;
-    projectListQueryResult: UseQueryResult<ProjectList, Error> | null;
-    projectQueryResult: UseQueryResult<Project, Error> | null;
+  componentQueryResult: UseQueryResult<Component, Error> | null;
+  componentListQueryResult: UseQueryResult<ComponentList, Error> | null;
+  projectListQueryResult: UseQueryResult<ProjectList, Error> | null;
+  projectQueryResult: UseQueryResult<Project, Error> | null;
 }
 
 export const GlobalStateContext = createContext<GlobalState>({
-    componentQueryResult: null,
-    componentListQueryResult: null,
-    projectListQueryResult: null,
-    projectQueryResult: null,
+  componentQueryResult: null,
+  componentListQueryResult: null,
+  projectListQueryResult: null,
+  projectQueryResult: null,
 });
 
-export function GlobalStateProvider({ children }: { children: React.ReactNode }) {
-    const projectHandle = useProjectHandle();
-    const componentHandle = useComponentHandle();
+export function GlobalStateProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const projectHandle = useProjectHandle();
+  const componentHandle = useComponentHandle();
 
-    const componentQueryResult = useComponent(projectHandle, componentHandle);
-    const componentListQueryResult = useComponentList(projectHandle);
-    const projectListQueryResult = useProjectList();
-    const projectQueryResult = useProject(projectHandle);
+  const componentQueryResult = useComponent(projectHandle, componentHandle);
+  const componentListQueryResult = useComponentList(projectHandle);
+  const projectListQueryResult = useProjectList();
+  const projectQueryResult = useProject(projectHandle);
 
-    return (
-        <GlobalStateContext.Provider value={{ componentQueryResult, componentListQueryResult, projectListQueryResult, projectQueryResult }}>
-            {children}
-        </GlobalStateContext.Provider>
-    );
+  return (
+    <GlobalStateContext.Provider
+      value={{
+        componentQueryResult,
+        componentListQueryResult,
+        projectListQueryResult,
+        projectQueryResult,
+      }}
+    >
+      {children}
+    </GlobalStateContext.Provider>
+  );
 }
