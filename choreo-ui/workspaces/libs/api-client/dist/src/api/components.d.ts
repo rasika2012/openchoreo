5b4a219755837ff800631e25af510b4853d53dcc
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-07-04 14:36:15 +0530

    Remove deprecated API client files and interfaces from the distribution directory
    - Deleted index.d.ts, index.js, and index.js.map files.
    - Removed components, deployments, general, and projects API files and their respective type definitions.
    - Eliminated core client and config files, including associated error handling and request functions.
    - Cleared out types definitions for Condition, Metadata, Project, Component, Deployment, and EndpointsResponse.

--- choreo-ui/workspaces/libs/api-client/dist/src/api/components.d.ts
+++
@@ -1,6 +0,0 @@
-import { ApiConfig } from '../core/config';
-import { ComponentList } from '../types/types';
-export interface ComponentsApi {
-    listProjectComponents(projectName: string, config?: ApiConfig): Promise<ComponentList>;
-}
-export declare const componentsApi: ComponentsApi;
--- choreo-ui/workspaces/libs/api-client/dist/src/api/projects.d.ts
+++
@@ -1,7 +0,0 @@
-import { ApiConfig } from '../core/config';
-import { Project, ProjectList } from '../types/types';
-export interface ProjectsApi {
-    listProjects(config?: ApiConfig): Promise<ProjectList>;
-    getProject(projectName: string, config?: ApiConfig): Promise<Project>;
-}
-export declare const projectsApi: ProjectsApi;
