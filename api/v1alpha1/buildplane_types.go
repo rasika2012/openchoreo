39caab9643fab73b6d892b15e21ab12b7f18f3cb
chalindukodikara <chalindu@wso2.com>
2025-07-04 16:05:35 +0530

    Add build plane

---
+++ api/v1alpha1/buildplane_types.go
@@ -0,0 +1,58 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// BuildPlaneSpec defines the desired state of BuildPlane.
+type BuildPlaneSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Owner BuildPlaneOwner `json:"owner"`
+
+	// KubernetesCluster defines the Kubernetes cluster where build workloads (e.g., Argo Workflows) will be executed.
+	KubernetesCluster KubernetesClusterSpec `json:"kubernetesCluster"`
+}
+
+type BuildPlaneOwner struct {
+	// +kubebuilder:validation:MinLength=1
+	OrganizationName string `json:"organizationName"`
+}
+
+// BuildPlaneStatus defines the observed state of BuildPlane.
+type BuildPlaneStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// BuildPlane is the Schema for the buildplanes API.
+type BuildPlane struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BuildPlaneSpec   `json:"spec,omitempty"`
+	Status BuildPlaneStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// BuildPlaneList contains a list of BuildPlane.
+type BuildPlaneList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BuildPlane `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BuildPlane{}, &BuildPlaneList{})
+}
---
+++ internal/controller/buildplane/buildplane_controller.go
@@ -0,0 +1,50 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package buildplane
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	corev1 "github.com/openchoreo/openchoreo/api/v1"
+)
+
+// BuildPlaneReconciler reconciles a BuildPlane object
+type BuildPlaneReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=core.choreo.dev,resources=buildplanes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core.choreo.dev,resources=buildplanes/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core.choreo.dev,resources=buildplanes/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the BuildPlane object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
+func (r *BuildPlaneReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = logf.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *BuildPlaneReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.BuildPlane{}).
+		Named("buildplane").
+		Complete(r)
+}
