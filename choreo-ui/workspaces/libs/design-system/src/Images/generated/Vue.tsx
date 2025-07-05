633d4df9f14b26f19e2a9ea73e6c85137e7744de
Rasika Maduranga <rasikat77@gmail.com>
2025-07-05 17:16:50 +0530

    Fix Image genarator sizing issue

--- choreo-ui/workspaces/libs/design-system/src/Images/generated/Vue.tsx
+++ choreo-ui/workspaces/libs/design-system/src/Images/generated/Vue.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
- 
+/* eslint-disable max-len */
 
 import * as React from 'react';
 
@@ -7,7 +7,7 @@ import * as React from 'react';
 export default function (props: React.SVGProps<SVGSVGElement>){
   return (
     <svg {...props}>
-      <g><React.Fragment><path fill="#41B883" d="m31.7723 3.6983-5.7735 10-5.7735-10H.9988l25 43.3017 25-43.3017z" /><path fill="#34495E" d="m31.7723 3.6983-5.7735 10-5.7735-10h-9.2265l15 25.9805 15-25.9805z" /></React.Fragment></g>
+      <g><React.Fragment><svg fill="none" viewBox="0 0 51 50"><path fill="#41B883" d="m31.7723 3.6983-5.7735 10-5.7735-10H.9988l25 43.3017 25-43.3017z" /><path fill="#34495E" d="m31.7723 3.6983-5.7735 10-5.7735-10h-9.2265l15 25.9805 15-25.9805z" /></svg></React.Fragment></g>
     </svg>
   )
 }
