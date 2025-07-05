448b1049e326b738b8021c7b65c0da0935f7860c
Rasika Maduranga <rasikat77@gmail.com>
2025-07-05 17:15:17 +0530

    Clean up console app structure and remove unused files

--- choreo-ui/workspaces/apps/console/src/App.tsx
+++ choreo-ui/workspaces/apps/console/src/App.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider, Box } from "@open-choreo/design-system";
 import { MainLayout } from "./layouts/MainLayout";
 import { useEffect } from "react";
-import { pluginRegistry } from "./plugins";
 import { useRouteExtentions, ExtentionProviderMounter } from "@open-choreo/plugin-core";
 
 export default function App() {
