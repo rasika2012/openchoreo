9c96665ab71208c4ba9bc9554f5bb4705eb189ce
jhivandb <jhivanb@gmail.com>
2025-07-08 14:47:31 +0530

    Use controller runtime for kubeconfig

--- internal/openchoreo-api/clients/k8s.go
+++ internal/openchoreo-api/clients/k8s.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	choreoapiv1 "github.com/openchoreo/openchoreo/api/v1"
 )
 
 func NewK8sClient() (client.Client, error) {
-	config, err := rest.InClusterConfig()
+	config, err := ctrl.GetConfig()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create kubernetes config: %w", err)
 	}
