a8c73913f5b94ab3e24fc09378f28feeaf4c3ef4
Rasika Maduranga <rasikat77@gmail.com>
2025-07-09 11:46:43 +0530

    Improve top level selector - Show display name instead of name - Fixed fetching logic to pick selected project via project api

---
+++ choreo-ui/workspaces/libs/choreo-api/src/utils/resource.ts
@@ -0,0 +1,25 @@
+import { ProjectItem, Resource } from "../types/types";
+
+export function getResourceDisplayName(resource: Resource) {
+  return resource?.displayName || resource?.name;
+}
+
+export function getResourceDescription(resource: Resource) {
+  return resource?.description || "";
+}
+
+export function getResourceCreatedAt(resource: Resource) {
+  return resource?.createdAt || "";
+}
+
+export function getResourceStatus(resource: Resource) {
+  return resource?.status || "";
+}
+
+export function getResourceDeploymentPipeline(resource: ProjectItem) {
+  return resource?.deploymentPipeline || "";
+}
+
+export function getResourceName(resource: Resource) {
+  return resource?.name || "";
+}
