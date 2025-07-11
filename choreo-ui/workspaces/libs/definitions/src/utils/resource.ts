a9f03553753b6864159c53c7c4ae76eed2f52914
Rasika Maduranga <rasikat77@gmail.com>
2025-07-11 12:56:44 +0530

    Fix build issues

--- choreo-ui/workspaces/libs/definitions/src/utils/resource.ts
+++ choreo-ui/workspaces/libs/definitions/src/utils/resource.ts
@@ -1,4 +1,4 @@
-import { ProjectItem, Resource } from "../types/resource";
+import { type ProjectItem, type Resource } from "../types/resource";
 
 export function getResourceDisplayName(resource: Resource) {
   return resource?.displayName || resource?.name;
