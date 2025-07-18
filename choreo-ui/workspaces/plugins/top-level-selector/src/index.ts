75414700514ae33af0ddabb78fd893a5229153cd
Rasika Maduranga <rasikat77@gmail.com>
2025-07-18 10:00:32 +0530

    Update overview pages

--- choreo-ui/workspaces/plugins/top-level-selector/src/index.ts
+++ choreo-ui/workspaces/plugins/top-level-selector/src/index.ts
@@ -1,9 +1,9 @@
 import { type PluginManifest } from "@open-choreo/plugin-core";
 
-import { panel } from "./panel";
+import { topLevelSelector } from "./TopLevelSelector";
 
 export const levelSelectorPlugin = {
   name: "Top Level Selector",
   description: "Top Level Selector to select cell architecture levels.",
-  extensions: [panel],
+  extensions: [topLevelSelector],
 } as PluginManifest;
