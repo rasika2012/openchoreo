99a509c84a44a5ece5bd9d069c56f76bdb64f348
Chalindu Kodikara <chalindu@wso2.com>
2025-07-18 11:17:14 +0530

    Add workflow templates to argo project types

--- internal/dataplane/kubernetes/types/argoproj.io/workflow/v1alpha1/register.go
+++ internal/dataplane/kubernetes/types/argoproj.io/workflow/v1alpha1/register.go
@@ -34,6 +34,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&Workflow{},
 		&WorkflowList{},
+		&ClusterWorkflowTemplate{},
+		&ClusterWorkflowTemplateList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
