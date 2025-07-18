63f398000c69632461c0cc871021c60d258fd218
Chalindu Kodikara <chalindu@wso2.com>
2025-07-18 11:16:50 +0530

    Update component to have build configs

--- api/v1alpha1/componentv2_types.go
+++ api/v1alpha1/componentv2_types.go
@@ -17,11 +17,47 @@ type ComponentOwner struct {
 
 // ComponentV2Spec defines the desired state of ComponentV2.
 type ComponentV2Spec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Owner defines the ownership information for the component
 	Owner ComponentOwner `json:"owner"`
 
+	// Type specifies the component type (e.g., Service, WebApplication, etc.)
 	Type ComponentType `json:"type"`
+
+	// Build defines the build configuration for the component
+	Build BuildSpecInComponent `json:"build"`
+}
+
+// BuildSpecInComponent defines the build configuration for a component
+// This specification is used to create BuildV2 resources when builds are triggered
+type BuildSpecInComponent struct {
+	// Repository defines the source repository configuration where the component code resides
+	Repository BuildRepository `json:"repository"`
+
+	// TemplateRef defines the build template reference and configuration
+	// This references a ClusterWorkflowTemplate in the build plane
+	TemplateRef TemplateRef `json:"templateRef"`
+}
+
+// BuildRepository defines the source repository configuration for component builds
+type BuildRepository struct {
+	// URL is the repository URL where the component source code is located
+	// Example: "https://github.com/org/repo" or "git@github.com:org/repo.git"
+	URL string `json:"url"`
+
+	// Revision specifies the default revision configuration for builds
+	// This can be overridden when triggering specific builds
+	Revision BuildRevision `json:"revision"`
+
+	// AppPath is the path to the application within the repository
+	// This is relative to the repository root. Default is "." for root directory
+	AppPath string `json:"appPath"`
+}
+
+// BuildRevision defines the revision specification for component builds
+type BuildRevision struct {
+	// Branch specifies the default branch to build from
+	// This will be used when no specific commit is provided for a build
+	Branch string `json:"branch"`
 }
 
 // ComponentV2Status defines the observed state of ComponentV2.
