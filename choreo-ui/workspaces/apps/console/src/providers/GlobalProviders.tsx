1aa2a0d86e8fd51b9f6ff033db6154407de0d2f9
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-07-21 10:30:53 +0530

    refactor: restructure choreo-context library and update dependencies
    - Moved `choreo-context` from plugins to libs for better organization.
    - Updated `pnpm-lock.yaml` to reflect new paths and added dependencies for the `choreo-context` library.
    - Modified `rush.json` to point to the new project folder for `choreo-context`.
    - Updated imports in various files to accommodate the new structure.
    - Implemented hooks for client, color mode, global state, and organizations.
    - Created context providers for API client and global state management.
    - Added reducers for application state management.
    - Removed old plugin structure and integrated necessary components into the new library structure.
    - Added TypeScript configuration and ESLint setup for the new library.

--- choreo-ui/workspaces/apps/console/src/providers/GlobalProviders.tsx
+++ choreo-ui/workspaces/apps/console/src/providers/GlobalProviders.tsx
@@ -1,20 +1,27 @@
-import { PluginProvider, type PluginManifest } from "@open-choreo/plugin-core";
+import { type PluginManifest } from "@open-choreo/plugin-core";
+import { GlobalStateProvider } from "@open-choreo/choreo-context";
 import { coreExtensionPoints } from "@open-choreo/plugin-core";
 import { WrapperExtensionMounter } from "@open-choreo/plugin-core";
 import { IntlProvider } from "react-intl";
 import { BrowserRouter } from "react-router";
 
-
-export const GlobalProviders = async ({ children, pluginRegistry }: { children: React.ReactNode, pluginRegistry: PluginManifest[] }) => {
-    return (
-        <BrowserRouter basename="/">
-            <PluginProvider pluginRegistry={pluginRegistry} basePath={window.configs.apiServerBaseUrl}>
-                <WrapperExtensionMounter extensionPoint={coreExtensionPoints.globalProvider} >
-                    <IntlProvider locale="en">
-                        {children}
-                    </IntlProvider>
-                </WrapperExtensionMounter>
-            </PluginProvider>
-        </BrowserRouter>
-    );
+export const GlobalProviders = async ({
+  children,
+}: {
+  children: React.ReactNode;
+  pluginRegistry: PluginManifest[];
+}) => {
+  return (
+    <BrowserRouter basename="/">
+      <GlobalStateProvider
+      // pluginRegistry={pluginRegistry} basePath={window.configs.apiServerBaseUrl}
+      >
+        <WrapperExtensionMounter
+          extentionPoint={coreExtensionPoints.globalProvider}
+        >
+          <IntlProvider locale="en">{children}</IntlProvider>
+        </WrapperExtensionMounter>
+      </GlobalStateProvider>
+    </BrowserRouter>
+  );
 };
