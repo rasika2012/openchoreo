0cdbbd4d82ecac2f812b75a702033d426f317fc9
Rasika Maduranga <rasikat77@gmail.com>
2025-07-21 10:35:40 +0530

    Update Contributing Guidelines

--- choreo-ui/workspaces/plugins/overview/dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { type PluginManifest } from "@open-choreo/plugin-core";
-export { organizationOverviewMainExtensionPoint, organizationOverviewSecondaryExtensionPoint, } from "./OrgOverview";
-export { projectOverviewMainExtensionPoint, projectOverviewSecondaryExtensionPoint, } from "./ProjectOverview";
-export { componentOverviewMainExtensionPoint } from "./ComponentOverview";
-export declare const overviewPlugin: PluginManifest;
