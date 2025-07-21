eef9e75f95bc3f76c86ab5bfed539dbc06c2fe01
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-07-21 10:30:53 +0530

    refactor: restructure choreo-context library and update dependencies
    - Moved `choreo-context` from plugins to libs for better organization.
    - Updated `pnpm-lock.yaml` to reflect new paths and added dependencies for the `choreo-context` library.
    - Modified `rush.json` to point to the new project folder for `choreo-context`.
    - Updated imports in various files to accommodate the new structure.
    - Implemented hooks for client, color mode, global state, and organizations.
    - Created context providers for API client and global state management.
    - Added reducers for application state management.
    - Removed old plugin structure and integrated necessary components into the new library structure.
    - Added TypeScript configuration and ESLint setup for the new library.

---
+++ choreo-ui/workspaces/libs/choreo-context/src/index.ts
@@ -0,0 +1,12 @@
+// import { type PluginManifest } from "@open-choreo/plugin-core";
+
+// import { provider } from "./providers";
+
+// export const choreoContextPlugin = {
+//   name: "Choreo Context",
+//   description: "Choreo Context and State Management Plugin",
+//   extensions: [provider],
+// } as PluginManifest;
+
+export * from "./hooks";
+export * from "./providers";
